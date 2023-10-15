322f417b9a61637fefa099519b1ee9c9914c5733
blackghost <117948300+IT-DIVIDMARK@users.noreply.github.com>
2023-10-15 06:15:36 -0700

    update 15/10/2023

--- ins1.py
+++ ins1.py
@@ -1,5 +1,6 @@
 import sys
 import time
+from random import choice
 from instagram_private_api import Client
 
 # Handle command line arguments for username and password
@@ -10,55 +11,50 @@ if len(sys.argv) < 3:
 user_name = str(sys.argv[1])
 password = str(sys.argv[2])
 
-# List of usernames to follow and unfollow
-usernames = [
-    'virat.kohli', 'narendramodi', 'aliaabhatt', 'deepikapadukone', 'shraddhakapoor',
-    'karanjohar', 'akshaykumar', 'priyankachopra', 'ranveersingh', 'rohitsharma45',
-    'tigerjackieshroff', 'kritisanon', 'varundvn'
-]
-
 try:
     # Initialize the Instagram Private API
     api = Client(user_name, password)
     print("\033[32m [•] login successful ")
 
     while True:
-        for username in usernames:
-            print(f"\n \033[1;33m Following {username} ....\033[0m")
+        # Random search query for verified accounts
+        search_query = "verified"
+        results = api.search_users(search_query)
+
+        if 'users' in results and len(results['users']) > 0:
+            random_user = choice(results['users'])
+            random_username = random_user['username']
+            print(f"\n \033[1;33m Following {random_username} ....\033[0m")
             time.sleep(3)
             try:
-                user_info = api.username_info(username)
-                user_id = user_info['user']['pk']
+                user_id = random_user['pk']
                 api.friendships_create(user_id)
 
                 # Get the user's recent posts
-                results = api.username_feed(user_id)
-                if 'items' in results:
-                    for post in results['items']:
+                user_feed = api.user_feed(user_id)
+                if 'items' in user_feed:
+                    for post in user_feed['items']:
                         media_id = post['id']
                         api.post_like(media_id)
                         print(f"Like action successful for post {media_id}")
                         time.sleep(45)
                         api.friendships_destroy(user_id)
-                        print(f"Unfollow action successful for {username}")
+                        print(f"Unfollow action successful for {random_username}")
 
             except Exception as e:
-                print(f"Error while interacting with {username}: {e}")
+                print(f"Error while interacting with {random_username}: {e}")
 
             print("Wait for 45 seconds, don't exit")
             time.sleep(45)
 
-        # Reinitialize the API for unfollowing
-        api = Client(user_name, password)
-        for username in usernames:
+            # Reinitialize the API for unfollowing
+            api = Client(user_name, password)
             try:
-                user_info = api.username_info(username)
-                user_id = user_info['user']['pk']
                 api.friendships_destroy(user_id)
-                print(f"Unfollow action successful for {username}")
+                print(f"Unfollow action successful for {random_username}")
                 time.sleep(3)
             except Exception as e:
-                print(f"Error while unfollowing {username}: {e}")
+                print(f"Error while unfollowing {random_username}: {e}")
 
         time.sleep(70)
 
