69a54b4b22db2284ce40985f997b32c95c6ad9c7
blackghost <117948300+IT-DIVIDMARK@users.noreply.github.com>
2023-10-15 06:32:25 -0700

    update

--- ins1.py
+++ ins1.py
@@ -17,46 +17,41 @@ try:
     print("\033[32m [•] login successful ")
 
     while True:
-        # Random search query for verified accounts
-        search_query = "verified"
-        results = api.search_users(search_query)
+        # Get online users
+        online_users = api.get_user_tags()
+        if online_users:
+            random_user = choice(online_users)
+            random_username = random_user['user']['username']
+            user_id = random_user['user']['pk']
 
-        if 'users' in results and len(results['users']) > 0:
-            random_user = choice(results['users'])
-            random_username = random_user['username']
             print(f"\n \033[1;33m Following {random_username} ....\033[0m")
             time.sleep(3)
-            try:
-                user_id = random_user['pk']
-                api.friendships_create(user_id)
 
-                # Get the user's recent posts
+            try:
+                api.friendships_create(user_id)
+                print(f"Follow action successful for {random_username}")
+
+                # Like only two posts
                 user_feed = api.user_feed(user_id)
                 if 'items' in user_feed:
+                    count = 0
                     for post in user_feed['items']:
-                        media_id = post['id']
-                        api.post_like(media_id)
-                        print(f"Like action successful for post {media_id}")
-                        time.sleep(45)
-                        api.friendships_destroy(user_id)
-                        print(f"Unfollow action successful for {random_username}")
+                        if count < 2:
+                            media_id = post['id']
+                            api.post_like(media_id)
+                            print(f"Like action successful for post {media_id}")
+                            count += 1
+                            time.sleep(15)  # Adjust the sleep time if needed
+
+                time.sleep(45)
+
+                api.friendships_destroy(user_id)
+                print(f"Unfollow action successful for {random_username}")
 
             except Exception as e:
                 print(f"Error while interacting with {random_username}: {e}")
 
-            print("Wait for 45 seconds, don't exit")
-            time.sleep(45)
-
-            # Reinitialize the API for unfollowing
-            api = Client(user_name, password)
-            try:
-                api.friendships_destroy(user_id)
-                print(f"Unfollow action successful for {random_username}")
-                time.sleep(3)
-            except Exception as e:
-                print(f"Error while unfollowing {random_username}: {e}")
-
-        time.sleep(70)
+        time.sleep(5)  # Check every 5 seconds for online users
 
 except Exception as e:
     print(f"An error occurred: {e}")
